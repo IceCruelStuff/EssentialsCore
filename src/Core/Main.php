c997a9759dc53955709b5b5e0e19ea095a853e2d
xXNiceAssasinl0 <xXNiceAssasinl0@users.noreply.github.com>
2017-12-13 21:18:03 -0500

    Added Feed,Heal

--- src/Core/Main.php
+++ src/Core/Main.php
@@ -9,6 +9,8 @@ use pocketmine\plugin\PluginBase;
 
 #Commands
 use Core\Commands\Fly;
+use Core\Commands\Feed;
+use Core\Commands\Heal;
 
 class Main extends PluginBase{
 
@@ -20,9 +22,18 @@ class Main extends PluginBase{
 
 	public function onEnable(){
 		self::$instance = $this;
-		$this->getServer()->getCommandMap()->register("fly", new fly("fly", $this));
+
+		$this->registerCommands();
+
 		$this->getLogger()->info("§aEnabled.");
 	}
+
+    private function registerCommands(){
+                    /////////////////////////////// COMMANDS ///////////////////////////////
+		$this->getServer()->getCommandMap()->register("fly", new fly("fly", $this));
+		$this->getServer()->getCommandMap()->register("feed", new feed("feed", $this));
+		$this->getServer()->getCommandMap()->register("heal", new heal("heal", $this));
+    }
 
 	public function onDisable(){
 	    $this->getLogger()->info("§cDisabled.");

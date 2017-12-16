2f05d18c15e0e7937b10aedb278fc43e41811fa5
xXNiceAssasinl0 <xXNiceAssasinl0@users.noreply.github.com>
2017-12-16 16:33:27 -0500

    Added Ops

--- src/Core/Main.php
+++ src/Core/Main.php
@@ -15,6 +15,7 @@ use Core\Commands\Spawn;
 use Core\Commands\SetSpawn;
 use Core\Commands\Ping;
 use Core\Commands\KickAll;
+use Core\Commands\Ops;
 
 class Main extends PluginBase{
 
@@ -41,6 +42,7 @@ class Main extends PluginBase{
 	        $this->getServer()->getCommandMap()->register("setspawn", new SetSpawn("setspawn", $this));
 	        $this->getServer()->getCommandMap()->register("ping", new Ping("ping", $this));
 	        $this->getServer()->getCommandMap()->register("kickall", new KickAll("kickall", $this));
+	        $this->getServer()->getCommandMap()->register("ops", new Ops("ops", $this));
     }
 
 	public function onDisable(){

ea501a6872a4cf71f218c270e43e3c65231e7fe6
xXNiceAssasinl0 <xXNiceAssasinl0@users.noreply.github.com>
2017-12-16 23:47:21 -0500

    Added /Top

--- src/Core/Main.php
+++ src/Core/Main.php
@@ -11,11 +11,14 @@ use pocketmine\plugin\PluginBase;
 use Core\Commands\Fly;
 use Core\Commands\Feed;
 use Core\Commands\Heal;
-use Core\Commands\Spawn;
-use Core\Commands\SetSpawn;
 use Core\Commands\Ping;
 use Core\Commands\KickAll;
 use Core\Commands\Ops;
+
+#Teleport
+use Core\Commands\Teleport\Spawn;
+use Core\Commands\Teleport\SetSpawn;
+use Core\Commands\Teleport\Top;
 
 class Main extends PluginBase{
 
@@ -29,24 +32,30 @@ class Main extends PluginBase{
 		self::$instance = $this;
 
 		$this->registerCommands();
+		$this->registerTeleportCommands();
 
 		$this->getLogger()->info("§aEnabled.");
 	}
 
     private function registerCommands(){
                     /////////////////////////////// COMMANDS ///////////////////////////////
-		$this->getServer()->getCommandMap()->register("fly", new Fly("fly", $this));
-		$this->getServer()->getCommandMap()->register("feed", new Feed("feed", $this));
-		$this->getServer()->getCommandMap()->register("heal", new Heal("heal", $this));
-	        $this->getServer()->getCommandMap()->register("spawn", new Spawn("spawn", $this));
-	        $this->getServer()->getCommandMap()->register("setspawn", new SetSpawn("setspawn", $this));
+	        $this->getServer()->getCommandMap()->register("fly", new Fly("fly", $this));
+	        $this->getServer()->getCommandMap()->register("feed", new Feed("feed", $this));
+	        $this->getServer()->getCommandMap()->register("heal", new Heal("heal", $this));
 	        $this->getServer()->getCommandMap()->register("ping", new Ping("ping", $this));
 	        $this->getServer()->getCommandMap()->register("kickall", new KickAll("kickall", $this));
 	        $this->getServer()->getCommandMap()->register("ops", new Ops("ops", $this));
     }
 
+    private function registerTeleportCommands(){
+                    /////////////////////////////// TELEPORT ///////////////////////////////
+	        $this->getServer()->getCommandMap()->register("spawn", new Spawn("spawn", $this));
+	        $this->getServer()->getCommandMap()->register("setspawn", new SetSpawn("setspawn", $this));
+	        $this->getServer()->getCommandMap()->register("top", new Top("top", $this));
+    }
+
 	public function onDisable(){
 	    $this->getLogger()->info("§cDisabled.");
-	}
+    }
 }
 
